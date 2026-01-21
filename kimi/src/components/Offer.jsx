9624987225b61aaac40d4af465373979c5778957
chriskunda <christianira2003@gmail.com>
2026-01-21 09:47:21 +0200

    cards bit done

--- kimi/src/components/Offer.jsx
+++ kimi/src/components/Offer.jsx
@@ -4,6 +4,7 @@ import Used from '../assets/usedtyres.jpg'
 import Pencage from '../assets/pencage.jpg'
 import Oil from '../assets/oilchange.jpg'
 import Rims from '../assets/newrims.webp'
+import Accessories from '../assets/accessories.jpg'
 
 
 const Offer = () => {
@@ -22,7 +23,7 @@ const Offer = () => {
                         </div>
                         <div className='card-description'>
                             <h1>Used tyres</h1>
-                            <p>All sizes and types of tyres from Europe and Dubai</p>
+                            <p>Inspected used tyres for safe driving</p>
                             <button className='card-description-button'>View items</button>
                         </div>
 
@@ -35,7 +36,7 @@ const Offer = () => {
                         </div>
                         <div className='card-description'>
                             <h1>New tyres</h1>
-                            <p>All sizes and types of tyres from around the world</p>
+                            <p>Quality new tyres you can trust.</p>
                             <button className='card-description-button'>View items</button>
                         </div>
                     </div>
@@ -47,7 +48,7 @@ const Offer = () => {
                         </div>
                         <div className='card-description'>
                             <h1>New Rims</h1>
-                            <p>All sizes and types of tyres from Europe and Dubai</p>
+                            <p>A global collection of rims in every size and style</p>
                             <button className='card-description-button'>View items</button>
                         </div>
                     </div>
@@ -58,7 +59,7 @@ const Offer = () => {
                         </div>
                         <div className='card-description'>
                             <h1>Wheel allignment</h1>
-                            <p>All sizes and types of tyres from Europe and Dubai</p>
+                            <p>Wheel alignment for better handling and tyre life</p>
                             <button className='card-description-button'>View items</button>
                         </div>
                     </div>
@@ -70,18 +71,18 @@ const Offer = () => {
                         </div>
                         <div className='card-description'>
                             <h1>Oil change</h1>
-                            <p>All sizes and types of tyres from Europe and Dubai</p>
+                            <p>Fresh oil. Better performance. Longer engine life.</p>
                             <button className='card-description-button'>View items</button>
                         </div>
                     </div>
 
                     <div className="item-card">
                         <div className='card-foto'>
-                            <img src={Oil} alt="" />
+                            <img src={Accessories} alt="" />
                         </div>
                         <div className='card-description'>
-                            <h1>Oil change</h1>
-                            <p>All sizes and types of tyres from Europe and Dubai</p>
+                            <h1>Accessories</h1>
+                            <p>Key accessories for car maintenance</p>
                             <button className='card-description-button'>View items</button>
                         </div>
                     </div>
