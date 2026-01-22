2d1e437f8cb4f7f428d3c1935f0f1bf1251aaa54
chriskunda <christianira2003@gmail.com>
2026-01-22 18:43:46 +0200

    FAQ

---
+++ kimi/src/components/Faq.jsx
@@ -0,0 +1,42 @@
+
+
+const Faq = () =>{
+    return(
+        <div className="parent-faq">
+
+            <div className="faq-leftside">
+                <h1>FAQ</h1>
+                <p>Frequently asked questions about us. You can <br />
+                    find the answers to these common questions in <br />
+                    the list of answers on the right side.
+                </p>
+                <button>More questions</button>
+            </div>
+
+            <div className="faq-rightside">
+                
+                <div>
+
+                    <p>What is Kimi?</p>
+                    <p>Kimi is an automobile and accessories hub that provides <br />
+                        cars with Used & new tyres of high quality, car accessories and <br />
+                        services of oil changing and wheel alignment under one loof.
+                    </p>
+
+                </div>
+                <div>
+                    
+                    <p>What is our guarantee policy?</p>
+                    <p>qwertyuikjh</p>
+                    
+                </div>
+                <div><p>Do we give our clients an EBM receipt?</p></div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Faq;
+
