262b11679fca74cc3756bdceb46bcda2717859e6
chriskunda <christianira2003@gmail.com>
2026-01-24 01:00:54 +0200

    faq bit done

--- kimi/src/App.jsx
+++ kimi/src/App.jsx
@@ -2,6 +2,7 @@ import Nav from './components/Header'
 import Products from './components/Product'
 import Offer from './components/Offer'
 import Feature from './components/Features'
+import Faq from './components/Faq'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Products/>
       <Offer/>
       <Feature/>
+      <Faq/>
     </div>
   )
 }
--- kimi/src/components/Faq.jsx
+++ kimi/src/components/Faq.jsx
@@ -1,4 +1,4 @@
-
+import './Faq.css'
 
 const Faq = () =>{
     return(
@@ -8,29 +8,34 @@ const Faq = () =>{
                 <h1>FAQ</h1>
                 <p>Frequently asked questions about us. You can <br />
                     find the answers to these common questions in <br />
-                    the list of answers on the right side.
+                    the list of answers on the right side by hovering on the question.
                 </p>
-                <button>More questions</button>
+                <button className='faq-leftside-button'>More questions</button>
             </div>
 
             <div className="faq-rightside">
                 
-                <div>
+                <div className='faq-rightside-div1'>
 
-                    <p>What is Kimi?</p>
-                    <p>Kimi is an automobile and accessories hub that provides <br />
+                    <p className='faq-rightside-div1-p1'>What is Kimi?</p>
+                    <p className='faq-rightside-div1-p2'>Kimi is an automobile and accessories hub that provides <br />
                         cars with Used & new tyres of high quality, car accessories and <br />
                         services of oil changing and wheel alignment under one loof.
                     </p>
 
                 </div>
-                <div>
+                <div className='faq-rightside-div2'>
                     
-                    <p>What is our guarantee policy?</p>
-                    <p>qwertyuikjh</p>
+                    <p className='faq-rightside-div2-p1'>What is our guarantee policy?</p>
+                    <p className='faq-rightside-div2-p2'>qwertyuikjh</p>
                     
                 </div>
-                <div><p>Do we give our clients an EBM receipt?</p></div>
+                <div className='faq-rightside-div3'>
+                    
+                    <p className='faq-rightside-div3-p1'>Do we give our clients an EBM receipt?</p>
+                    <p className='faq-rightside-div3-p2'>astyuikxc</p>
+                    
+                </div>
 
             </div>
 
