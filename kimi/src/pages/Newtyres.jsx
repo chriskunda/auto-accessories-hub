11437f9cbf53db8f3dd51700c184bc597658ee89
chriskunda <christianira2003@gmail.com>
2026-02-13 13:50:26 +0200

    starting

--- kimi/src/pages/Newtyres.jsx
+++ kimi/src/pages/Newtyres.jsx
@@ -2,9 +2,49 @@ import './Newtyres.css'
 
 
 const Newtyres = () => {
+    return(
     <div className='newtyres-parent'>
-        <h1>braaaaaaaaa</h1>
+        
+        <div className='sub-newtyres-parent'>
+
+            <div className='newty-description'>
+                <h1>NEW TYRES</h1>
+                <p>Below there is variety of brands of the new tyres we provide. Take a look and add to cart whichever you would like to purchase.</p>
+            </div>
+
+            <div className='crads-newty'>
+
+                <div className='card1'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <div className='card2'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <div className='card3'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <div className='card4'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <div className='card5'>
+                    <div></div>
+                    <div></div>
+                </div>
+
+            </div>
+
+        </div>
+        
     </div>
+    )
 }
 
 export default Newtyres;
