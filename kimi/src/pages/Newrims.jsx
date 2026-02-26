a2bd09798fa92d54b4e74b0c43ec31d7fc149a18
chriskunda <christianira2003@gmail.com>
2026-02-26 22:15:59 +0200

    rims not done

--- kimi/src/pages/Newrims.jsx
+++ kimi/src/pages/Newrims.jsx
@@ -4,26 +4,32 @@ import './Newrims.css'
 const Newrims = () => {
         return(
             <div className="rims-parent">
-                <div className="rims-decsri">
-                    <h1>New Rims</h1>
-                    <p>alot more of them</p>
+
+                <div className='sub-rims-parent'>
+
+                    <div className="rims-decsri">
+                        <h1>New Rims</h1>
+                        <p>alot more of them</p>
+                    </div>
+
+                    <div className="rims-cards">
+
+                        <div className="card2"></div>
+
+                        <div className="card2"></div>
+
+                        <div className='card2'></div>
+
+                        <div className='card2'></div>
+
+                        <div className='card2'></div>
+
+                        <div className='card2'></div>
+
+                    </div>
+
                 </div>
-
-                <div className="rims-cards">
-
-                    <div className="card2"></div>
-
-                    <div className="card2"></div>
-
-                    <div className='card2'></div>
-
-                    <div className='card2'></div>
-
-                    <div className='card2'></div>
-
-                    <div className='card2'></div>
-
-                </div>
+                
             </div>
         )
 }
