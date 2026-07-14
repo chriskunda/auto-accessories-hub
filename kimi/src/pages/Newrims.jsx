982864dfc89195875a0949ad419a0d77afbfee81
chriskunda <christianira2003@gmail.com>
2026-07-14 22:25:49 +0200

    ..

--- kimi/src/pages/Newrims.jsx
+++ kimi/src/pages/Newrims.jsx
@@ -7,11 +7,7 @@ import {rims} from '../objects/rims'
 
 const Newrims = () => {
     
-    // const [rims] = useState([
-    //     {id:1, name: "Grey", price: "1,200,000", size: "18\"", quantity:"One set"},
-    //     {id:1, name: "Silver", price: "1,200,000", size: "18\"", quantity:"Two sets"},
-    //     {id:1, name: "Golden", price: "1,500,000", size: "19\"", quantity:"One set"}
-    // ])
+    
     
 
 
