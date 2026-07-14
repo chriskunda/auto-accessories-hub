f740c0ef8426ef606a913afd162e3959f9053911
chriskunda <christianira2003@gmail.com>
2026-07-14 22:15:41 +0200

    ...

--- kimi/src/objects/rims.js
+++ kimi/src/objects/rims.js
@@ -2,5 +2,6 @@
 
 export const rims = [
     {id:1, name: "Grey", price: "1,200,000", size: "18\"", quantity:"One set"},
+    {id:1, name: "Silver", price: "1,200,000", size: "18\"", quantity:"Two sets"},
 
 ]
