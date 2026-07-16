698a9419a43cc5244c35b948f553e2bade139445
chriskunda <christianira2003@gmail.com>
2026-07-16 19:44:51 +0200

    ....

--- kimi/src/objects/rims.js
+++ kimi/src/objects/rims.js
@@ -3,5 +3,5 @@ import tyre from '../assets/tyre.png'
 export const rims = [
     {id:1, name: "Grey", price: "1,200,000", size: "18\"", quantity:"One set", image: tyre},
     {id:2, name: "Silver", price: "1,200,000", size: "18\"", quantity:"Two sets", image: tyre},
-    {id:3, name: "Golden", price: "1,500,000", size: "19\"", quantity:"One set", image: tyre},
+    {id:3, name: "Golden", price: "1,500,000", size: "19\"", quantity:"One set", image: ''},
     ]
