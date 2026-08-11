491be4ac0156f3ca34d696ec8aeb8e127d70d263
chriskunda <christianira2003@gmail.com>
2026-08-11 12:46:58 +0200

    done

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import { useCart } from '../context/CartContext'
 import './Header.css'
 import { BsCart3 } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
 import Cart from '../pages/Cart'
 
 
 const Nav = () => {
 
     const { cartOpen, openCart, closeCart, getCartCount } = useCart();
+    const navigate = useNavigate();
+
+    const navigateHomeHash = (hash, e) => {
+        e.preventDefault();
+        navigate(`/${hash}`);
+    };
 
     return(
         <nav>
@@ -17,8 +24,8 @@ const Nav = () => {
                 <div className='links'>
                     <a href="/">Home</a>
                     <a href="">About</a>
-                    <a href="#services">Services</a>
-                    <a href="#call">Contact</a>
+                    <a href="/#services" onClick={(e) => navigateHomeHash('#services', e)}>Services</a>
+                    <a href="/#call" onClick={(e) => navigateHomeHash('#call', e)}>Contact</a>
                 </div>
                 <div className='AddtoCart'>
 
